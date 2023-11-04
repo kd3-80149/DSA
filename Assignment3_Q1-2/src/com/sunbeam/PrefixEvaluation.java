7988c940fa8ec49bbd5fc5ccf1c997f8df4f61d2
Anita-parmar <parmaranu111@gmail.com>
2023-11-04 17:53:25 +0530

    Assignment3-Q1-2

---
+++ Assignment3_Q1-2/src/com/sunbeam/PrefixEvaluation.java
@@ -0,0 +1,10 @@
+package com.sunbeam;
+
+public class PrefixEvaluation {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Assignment3_Q1-2/src/com/sunbeam/Stack.java
@@ -0,0 +1,40 @@
+package com.sunbeam;
+
+public class Stack {
+	private int arr[];
+	private int top;
+	private final int SIZE;
+
+	public Stack(int size) {
+		SIZE = size;
+		arr = new int[SIZE];
+		top = -1;
+	}
+
+	public void push(int data) {
+		top++;
+		arr[top] = data;
+
+	}
+
+	public int pop() {
+		int data = arr[top];
+		top--;
+		return data;
+
+	}
+
+	public int peek() {
+		return arr[top];
+
+	}
+
+	public boolean isFull() {
+		return top == SIZE - 1;
+	}
+
+	public boolean isEmpty() {
+		return top == -1;
+	}
+
+}
